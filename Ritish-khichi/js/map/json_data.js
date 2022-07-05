363a97b659a36ca5e37b09bf274c67cb7ad45c8a
ritishkhichi <104276413+ritishkhichi@users.noreply.github.com>
2022-07-05 13:34:49 +0530

    Add files via upload

---
+++ Ritish-khichi/js/map/json_data.js
@@ -0,0 +1,116 @@
+eqfeed_callback({
+
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "properties": {
+                "place": "Rohtak,Haryana -YC",
+                "βvalue": 2.07,
+                "R2": 0.994,
+                "bvalue": 0.731,
+                "bvalueR2": 0.81,
+                "alert": "green",
+                "γr1": 0.13,
+                "αvalue": 0.872,
+                "time ": 1652659301
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    28.890997,
+                    76.60233
+                ]
+            },
+            "id": "usc000csw5"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "place": "Rewari ,Haryana- YF",
+                "βvalue": 1.241,
+                "R2": 0.991,
+                "bvalue": 0.794,
+                "bvalueR2": 0.924,
+                "alert": "#800d00",
+                "γr1": 0.14,
+                "αvalue": 0.906,
+                "time ": 1652659376
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    28.193533,
+                    76.617648
+                ]
+            },
+            "id": "usc000csw5"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "place": "Guwahati,Assam-BP\n\n",
+                "βvalue": 1.184,
+                "R2": 0.965,
+                "bvalue": 0.653,
+                "bvalueR2": 0.702,
+                "alert": "#004e80",
+                "γr1": 0.12,
+                "αvalue": 1.09,
+                "time ": 1652655960
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.174722,
+                    91.774975
+                ]
+            },
+            "id": "usc000csw5"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "place": "Barpeta,Assam-BG",
+                "βvalue": 1.097,
+                "R2": 0.976,
+                "b value": 0.698,
+                "bvalueR2": 0.68,
+                "alert": "#5c0080",
+                "γr1": 0.12,
+                "αvalue": 1.014,
+                "time ": 1652656128
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.32292,
+                    91.00632
+                ]
+            },
+            "id": "usc000csw5"
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "place": "Tihu,Nalbari district,Assam-RS and PC.\n\n",
+                "βvalue": "1.039,1.029",
+                "R2": "0.9968,0.9846",
+                "bvalue": "0.310,0.268",
+                "bvalueR2": "0.906,0.921",
+                "alert": "#807400",
+                "γr1": "0.08,0.06",
+                "αvalue": "1.310,1.392",
+                "time ": 1652650920
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    26.4758,
+                    91.2674
+
+                ]
+            },
+            "id": "usc000csw5"
+        }
+    ]
+});
